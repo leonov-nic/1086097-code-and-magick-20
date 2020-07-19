ea9b2c7147fd688c61b5ff407ea6b59c4cdcd5cc
Leonov-nic <leonov-nic@yandex.ru>
2020-07-19 22:19:51 +0300

    module6-task1

---
+++ js/backend.js
@@ -0,0 +1,74 @@
+'use strict';
+
+(function () {
+
+  var StatusCode = {
+    OK: 200
+  };
+  var TIMEOUT_IN_MS = 10000;
+
+  var load = function (onLoad, onError) {
+    var URL = 'https://javascript.pages.academy/code-and-magick/data';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Ошибка: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open('GET', URL);
+    xhr.send();
+  };
+
+  // console.log(xhr.status);
+  // console.log(xhr.readyState);
+  // console.log(xhr.response);
+  // console.log(xhr.responseText);
+
+  var save = function (data, onload, onerror) {
+    var URL = 'https://javascript.pages.academy/code-and-magick';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onload(xhr.response);
+      } else {
+        onerror('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onerror('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onerror('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open('POST', data);
+    xhr.send(URL);
+  };
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+
+})();
